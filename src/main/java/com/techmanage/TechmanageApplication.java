577dee334909b7d76da0e83538269a03e8f9cb74
WendelSReis <wendel.m.w18@gmail.com>
2025-04-30 21:42:59 -0300

    feat: inicializa com projeto spring boot

---
+++ src/main/java/com/techmanage/TechmanageApplication.java
@@ -0,0 +1,13 @@
+package com.techmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechmanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechmanageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techmanage/TechmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechmanageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
